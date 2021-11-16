eedb68adfb23b88f9861202848279f6e8a10132f
Valerie Russell <vruss14@gmail.com>
2021-11-15 19:01:26 -0500

    add modal to home screen

---
+++ public/assets/js/modal.js
@@ -0,0 +1,13 @@
+let modal = document.getElementById("modal");
+
+window.onload = function() {
+    modal.style.display = "block";
+}
+
+setTimeout(function(){
+    modal.style.display = "none";
+}, 2000);
+
+
+
+
